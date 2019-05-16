f5af4ca5549f97ba10f4f226603c0ef6ea7b2c79
rlp0019 <rlp0019@alu.ubu.es>
2019-05-16 13:26:38 +0200

    Cambiado el botón atrás y el contenido de about. Closes #58.

--- src/gui/EscenaAbout.java
+++ src/gui/EscenaAbout.java
@@ -19,23 +19,23 @@ public class EscenaAbout extends StackPane {
 		this.setMinSize(1000, 700);
 		this.setBackground(CreadorElementos.createBackground());
 
-		Label about = CreadorElementos.createLabel("Sobre mi", 32, "#383838", 0, 10);
+		Label about = CreadorElementos.createLabel("Sobre la universidad", 32, "#383838", 0, 10);
 
-		Label info = CreadorElementos.createLabel("Estudiante de Ingeniería Informática en la universidad de Burgos, \n"
-				+ "con especial inclinación hacia el desarrollo de software y videojuegos. \n"
-				+ "\nInteresado en el lenguaje Java.", 20, "#000000", 0, 50);
+		Label info = CreadorElementos.createLabel("Universidad pública situada en Burgos, creada en el 1994\n"
+				+ "como una partición de la Universidad de Valladolid \n" + "a partir del campus existente en Burgos.",
+				20, "#000000", 0, 25);
 
-		Image imagen = new Image(getClass().getClassLoader().getResource("imagenes/Roberto.jpg").toExternalForm());
+		Image imagen = new Image(getClass().getClassLoader().getResource("imagenes/Ubu.png").toExternalForm());
 		ImageView imagenV = new ImageView(imagen);
-		imagenV.setScaleX(1.5);
-		imagenV.setScaleY(1.5);
+		imagenV.setScaleX(2.5);
+		imagenV.setScaleY(2.5);
 		imagenV.setTranslateY(-150);
 
-		Button atrasB = CreadorElementos.createButton("Atrás", 16, "Volver a la pantalla anterior.", 5, -5, 100);
+		Button atrasB = CreadorElementos.createButton("Cerrar", 16, "Volver a la pantalla anterior.", 5, -5, 100);
 		atrasB.setOnAction(e -> aplicacion.cambiaEscena(0));
 
 		this.getChildren().addAll(about, info, imagenV, atrasB);
-		EscenaAbout.setAlignment(atrasB, Pos.BOTTOM_LEFT);
+		EscenaAbout.setAlignment(atrasB, Pos.BOTTOM_CENTER);
 		EscenaAbout.setAlignment(about, Pos.TOP_CENTER);
 		EscenaAbout.setAlignment(info, Pos.CENTER);
 		EscenaAbout.setAlignment(imagenV, Pos.CENTER);
